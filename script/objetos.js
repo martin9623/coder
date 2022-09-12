6f7e5832d5c66ddf06c176ba194bb9e5fd722231
Martin <martingomez.9623@gmail.com>
2022-09-11 21:41:21 -0300

    creado buscador de productos

--- script/objetos.js
+++ script/objetos.js
@@ -1,78 +1,104 @@
-let resultado = 0;
-const TABLA = document.querySelector('.tabla')
+const CARRITO = document.querySelector('.carrito')
+const RESULTADOS = document.querySelector('.buscador__resultados')
+const INPUT = document.querySelector('.buscador__input')
+const BTN = document.querySelector('.buscador__btn')
+const BUSCADOR_TITULOS = document.querySelector('.buscador__titulos')
+const CARRITO__EMPTY = document.querySelector('.carrito__empty')
 
 class Productos {
-    constructor(numeroProducto, producto, cantidad, precio) {
-        this.numeroProducto = numeroProducto
-        this.producto = producto
+    constructor(id, nombre, cantidad, precio, total) {
+        this.id = id
+        this.nombre = nombre
         this.cantidad = cantidad
         this.precio = precio
+        this.total = total
     }
 }
 
 let frutas = []
 
-const fruta1 = new Productos(1, 'manzana', '1 kg', 300)
-const fruta2 = new Productos(2, 'banana', '1 kg', 400)
-const fruta3 = new Productos(2, 'pera', '1 kg', 400)
-const fruta4 = new Productos(2, 'banana', '1 kg', 400)
-const fruta5 = new Productos(2, 'banana', '1 kg', 400)
-const fruta6 = new Productos(2, 'banana', '1 kg', 400)
-const fruta7 = new Productos(2, 'banana', '1 kg', 400)
-const fruta8 = new Productos(2, 'banana', '1 kg', 400)
-const fruta9 = new Productos(2, 'banana', '1 kg', 400)
+const manzana = new Productos(1, 'manzana', 1, 300, 0)
+const banana = new Productos(2, 'banana', 1, 400, 0)
+const pera = new Productos(3, 'pera', 1, 250, 0)
+const papa = new Productos(4, 'papa', 1, 100, 0)
+const zanahoria = new Productos(5, 'zanahoria', 1, 400, 0)
+const manzana_arenoza = new Productos(6, 'manzana arenoza', 1, 300, 0)
 
-frutas.push(fruta1, fruta2, fruta3, fruta4, fruta5, fruta6, fruta7, fruta8, fruta9)
+frutas.push(manzana, banana, pera, papa, zanahoria, manzana_arenoza)
 
-console.log(frutas)
+// frutas.forEach(producto => {
+//     CARRITO.innerHTML += crearTabla(producto.id, producto.nombre, producto.cantidad, producto.precio, producto.total)
+// })
 
-frutas.forEach(fruta => {
-    console.log(fruta);
-    TABLA.innerHTML += `
-            <tr>
-                <td>${fruta.numeroProducto}</td>
-                <td>${fruta.producto}</td>
-                <td>${fruta.cantidad}</td>
-                <td>${fruta.precio}</td>
-            </tr>
-    `
+function crearTabla(id, nombre, cantidad, precio, total) {
+    return `<tr>
+        <td>${id}</td>
+        <td>${nombre}</td>
+        <td>
+            <button class="btn__tabla">-</button>
+                <span class=num__cantidad>
+                    ${cantidad}
+                </span>
+                kg
+            <button class="btn__tabla">+</button>
+        </td>
+        <td class="precio">${precio}</td>
+        <td class="total">${precio}</td>
+        <td>
+            <button class="btn__tabla">Add to cart</button>
+        </td>
+    </tr>`
+}
+
+BTN.addEventListener('click', () => {
+    RESULTADOS.innerHTML = ``
+
+    let busqueda = INPUT.value.toLowerCase()
+
+    const resultado = buscar(frutas, busqueda)
+
+    resultado.forEach(el => {
+        RESULTADOS.innerHTML += crearTabla(el.id,
+            el.nombre,
+            el.cantidad,
+            el.precio,
+            el.total)
+    })
+
+    BUSCADOR_TITULOS.style.display = 'table-row'
 })
 
-
-
-const condicion = () => {
-
-    let respuesta = prompt('Quiere ingresar otro producto? Si/No')
-    respuesta = respuesta.toLowerCase()
-
-    if (respuesta === 'si') {
-        agregarProducto()
-    } else if (respuesta === 'no') {
-        alert(`Su total es de $${resultado}`)
-    } else {
-        alert('Por favor ingrese una respuesta correcta')
-        condicion()
-    }
-
+function buscar(array, busqueda) {
+    return array.filter((el) => el.nombre.includes(busqueda))
 }
 
-const agregarProducto = () => {
-    producto = prompt('Ingrese numero de producto a comprar')
-    producto = Number(producto)
+RESULTADOS.addEventListener('click', (e) => {
+    let event = e.target.textContent
+    let cantidad = e.target.parentNode.querySelector('.num__cantidad')
+    let precio = e.target.parentNode.parentNode.querySelector('.precio')
+    let total = e.target.parentNode.parentNode.querySelector('.total')
 
-    if (producto === 1) {
-        resultado += 200
-        condicion()
-    } else if (producto === 2) {
-        resultado += 300
-        condicion()
-    } else if (producto === 3) {
-        resultado += 250
-        condicion()
-    } else {
-        alert('Por favor ingrese una respuesta correcta')
-        agregarProducto()
+    if (event === '+') {
+        if (cantidad.innerHTML > 0) {
+            calculoProducto('sumar', cantidad, precio, total)
+        }
+    } else if (event === '-') {
+        if (cantidad.innerHTML > 1) {
+            calculoProducto('restar', cantidad, precio, total)
+        }
+    }
+})
+
+function calculoProducto(operacion, cantidad, precio, total) {
+    if (operacion === 'sumar') {
+        cantidad.innerHTML = Number(cantidad.innerHTML) + 1
+        calculoTotal()
+    } else if (operacion === 'restar') {
+        cantidad.innerHTML = Number(cantidad.innerHTML) - 1
+        calculoTotal()
+    }
+
+    function calculoTotal() {
+        total.innerHTML = Number(precio.innerHTML) * Number(cantidad.innerHTML)
     }
 }
-
-// window.onload = () => agregarProducto()
